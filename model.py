6e45859825af280e190994e77e508ae7e29e95fd
CGraciolli <cgraciolli@gmail.com>
2025-01-17 18:24:42 +0100

    batch type is now an enum

--- model.py
+++ model.py
@@ -1,25 +1,29 @@
 from dataclasses import dataclass
 from typing import Optional
 from datetime import date
+from enum import Enum, auto
+
+
+class BatchType(Enum):
+    WAREHOUSE = auto()
+    SHIPMENT = auto()
+
 
 
 @dataclass(frozen=True)
 class OrderLine:
     orderid: str
     sku: str
-    qty: int
-    
-    
+    qty: int 
+
     def choose_batch(self, batch1, batch2):
-                
-        ## really should be an Enumarate
         if batch1.can_allocate(self) and batch2.can_allocate(self):
-            if batch1.batch_type == batch2.batch_type:
+            if batch1.batch_type.value == batch2.batch_type.value:
                 if batch1.eta <= batch2.eta:
                     return batch1
                 else:
                     return batch2
-            elif batch1.batch_type == "warehouse":
+            elif batch1.batch_type.value < batch2.batch_type.value:
                 return batch1
             else:
                 return batch2
@@ -31,16 +35,14 @@ class OrderLine:
             return None
 
 
- ## TODO: shipment and warehouse shoul be an Enumerate
 class Batch:
-    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date] = None, batch_type: str = None):
+    def __init__(self, ref: str, sku: str, qty: int, batch_type: BatchType, eta: Optional[date] = None):
         self.reference = ref
         self.sku = sku
         self.qty = qty
         self.eta = eta
         self.batch_type = batch_type
         self.available_qty = qty
-    
     
     def can_allocate(self, order: OrderLine) -> bool:
         if self.sku == order.sku:
--- test_model.py
+++ test_model.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 import pytest
 
 # from model import ...
-from model import OrderLine, Batch
+from model import OrderLine, Batch, BatchType
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
@@ -11,7 +11,7 @@ later = tomorrow + timedelta(days=10)
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     order = OrderLine("order1", "sku1", 2)
-    batch = Batch("batch1", "sku1", 20)
+    batch = Batch("batch1", "sku1", 20, BatchType.SHIPMENT)
     
     batch.allocate(order)
     
@@ -20,37 +20,35 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
 
 def test_can_allocate_if_available_greater_than_required():
     order = OrderLine("order1", "sku1", 2)
-    batch = Batch("batch1", "sku1", 20)
+    batch = Batch("batch1", "sku1", 20, BatchType.SHIPMENT)
     
     assert batch.can_allocate(order)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
     order = OrderLine("order1", "sku1", 2)
-    batch = Batch("batch1", "sku1", 1)
+    batch = Batch("batch1", "sku1", 1, BatchType.SHIPMENT)
     
     assert not batch.can_allocate(order)
 
 
 def test_can_allocate_if_available_equal_to_required():
     order = OrderLine("order1", "sku1", 2)
-    batch = Batch("batch1", "sku1", 2)
+    batch = Batch("batch1", "sku1", 2, BatchType.SHIPMENT)
     
     assert batch.can_allocate(order)
     
 def test_cannot_allocalate_if_skus_dont_match():
     order = OrderLine("order1", "sku1", 2)
-    batch = Batch("batch1", "sku2", 2)
+    batch = Batch("batch1", "sku2", 2, BatchType.SHIPMENT)
     
     assert not batch.can_allocate(order)
 
 
 def test_prefers_warehouse_batches_to_shipments():
     order = OrderLine("order1", "sku1", 2)
-    batch1 = Batch("batch1", "sku1", 20, tomorrow, "shipment")
-    batch2 = Batch("batch1", "sku1", 20, tomorrow, "warehouse")
-    
-    ## TODO: shipment an warehouse shoul be an Enumerate
+    batch1 = Batch("batch1", "sku1", 20, BatchType.SHIPMENT, tomorrow)
+    batch2 = Batch("batch1", "sku1", 20, BatchType.WAREHOUSE, tomorrow)
     
     chosen_batch = order.choose_batch(batch1, batch2)
     
@@ -59,8 +57,8 @@ def test_prefers_warehouse_batches_to_shipments():
 
 def test_prefers_earlier_batches():
     order = OrderLine("order1", "sku1", 2)
-    batch1 = Batch("batch1", "sku1", 20, tomorrow)
-    batch2 = Batch("batch1", "sku1", 20, later)
+    batch1 = Batch("batch1", "sku1", 20, BatchType.SHIPMENT, tomorrow)
+    batch2 = Batch("batch1", "sku1", 20, BatchType.SHIPMENT, later)
     
     chosen_batch = order.choose_batch(batch1, batch2)
     
@@ -68,8 +66,8 @@ def test_prefers_earlier_batches():
 
 def test_doesnt_choose_unallocatable_batches():
     order = OrderLine("order1", "sku1", 2)
-    batch1 = Batch("batch1", "sku2", 20, tomorrow)
-    batch2 = Batch("batch1", "sku1", 20, later)
+    batch1 = Batch("batch1", "sku2", 20, BatchType.SHIPMENT, tomorrow)
+    batch2 = Batch("batch1", "sku1", 20, BatchType.SHIPMENT, later)
     
     chosen_batch = order.choose_batch(batch1, batch2)
     
@@ -77,8 +75,8 @@ def test_doesnt_choose_unallocatable_batches():
 
 def test_doesnt_choose_if_batches_cant_allocate():
     order = OrderLine("order1", "sku1", 2)
-    batch1 = Batch("batch1", "sku2", 20, tomorrow)
-    batch2 = Batch("batch1", "sku1", 1, later)
+    batch1 = Batch("batch1", "sku2", 20, BatchType.SHIPMENT, tomorrow)
+    batch2 = Batch("batch1", "sku1", 1, BatchType.SHIPMENT, later)
     
     chosen_batch = order.choose_batch(batch1, batch2)
     
